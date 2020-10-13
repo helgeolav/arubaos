d86c634b46b047780cf835f69937c1250753fcb9
drkchiloll <sam.womack@entropicos.com>
2020-10-13 11:49:47 -0500

    documentation|examples

--- cmd/main.go
+++ cmd/main.go
@@ -26,8 +26,30 @@ func main() {
 	}
 	defer client.Logout()
 
+	// intf, _ := client.GetApPortStatus("d0:d3:e0:c1:3b:e0")
+	wiredMac := "d0:d3:e0c1:3d:1e"
+	intf, _ := client.GetApPortStatus(wiredMac)
+	fmt.Println(intf)
+	// lldp, err := client.GetApLLDPInfo("ap01.mst.jeffersonn.705.mo")
+	// if err != nil {
+	// 	log.Fatalf("%v", err)
+	// }
+	// fmt.Println(lldp)
+	// ass, _ := client.GetApAssocCount("ap01.mst.jeffersonn.705.mo")
+	// fmt.Println(ass)
+}
+
+func something(client *arubaos.Client) {
+	f := arubaos.AFilter{
+		Count:   2000,
+		CfgPath: "/md/ResNet/Birmingham_Souhtern_College",
+	}
+
+	aps, _ := client.GetMMApDB(f)
+	fmt.Println(len(aps))
+
 	// ap, _ := client.GetAp("ap01.bsc.olin.102.al")
 	// fmt.Println(ap)
-	apAssocs, _ := client.GetApAssocCount("ap01.bsc.hilltopapt.33-2-c.al")
-	fmt.Println(apAssocs)
+	// apAssocs, _ := client.GetApAssocCount("ap01.bsc.hilltopapt.33-2-c.al")
+	// fmt.Println(apAssocs)
 }
